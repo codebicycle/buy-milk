008050adfbbf6812452b981be41de9df1a8bb2f0
codebicycle <codebicycle@gmail.com>
2017-07-06 23:11:56 +0300

    Refactor

--- todo/todo.py
+++ todo/todo.py
@@ -11,17 +11,13 @@ app.secret_key = SECRET_KEY
 @app.route('/')
 @app.route('/welcome')
 def welcome():
-    session_email = session.get('email')
-    return render_template('welcome.html', session_email=session_email)
+    return render_template('welcome.html')
 
 
 @app.route('/login', methods=['GET'])
 def sessions_new():
-    session_email = session.get('email')
     email = request.args.get('email')
-    return render_template('login_form.html',
-                            session_email=session_email,
-                            email=email)
+    return render_template('login_form.html', email=email)
 
 
 @app.route('/login', methods=['POST'])
@@ -29,12 +25,12 @@ def sessions_create():
     session.clear()
     email = request.form['email']
     password = request.form['password']
-    if are_valid_credentials(email, password):
+    if valid_credentials(email, password):
         session['email'] = email
-        return redirect( url_for('welcome'))
+        return redirect(url_for('welcome'))
     else:
         flash("Email and password do not match!", 'error')
-        return redirect( url_for('sessions_new', email=email))
+        return redirect(url_for('sessions_new', email=email))
 
 
 @app.route('/logout', methods=['GET'])
@@ -43,7 +39,7 @@ def sessions_destroy():
     return redirect(url_for('welcome'))
 
 
-def are_valid_credentials(email, password):
+def valid_credentials(email, password):
     credentials = {
         'user@example.com': 'test',
     }
