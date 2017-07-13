0cecfaa5f30ba1f2e57c2da0531df861ad000554
codebicycle <codebicycle@gmail.com>
2017-07-13 09:39:38 +0300

    Store configuration in separate file

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+try:
+    from todo.secrets import SECRET_KEY
+except ImportError:
+    if not os.environ.get('SECRET_KEY'):
+        raise ValueError('Secret key not set.')
+
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+SECRET_KEY = os.environ.get('SECRET_KEY') or SECRET_KEY
+SQLALCHEMY_DATABASE_URI = (
+	os.environ.get('DATABASE_URL') or
+    'sqlite:///' + os.path.join(base_dir, 'todo', 'development.sqlite3')
+)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
